8383f253da031ac2d19abcac1ef505d796d0b9ea
AfraaAlobaid <afraa.obaid1@gmail.com>
2022-02-13 19:43:37 +1030

    Added get_page to scraper & html page to fitures

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,2 +1,14 @@
-class Sraper
+require 'nokogiri'
+
+class Scraper
+    def initialize
+    end
+
+    def get_page
+        puts "Hey!"
+        #puts Nokogiri::HTML(open("https://bettereducation.com.au/school/primary/sa/adelaide_top_government_primary_schools.aspx"))
+    end
 end
+
+
+Scraper.new.get_page
